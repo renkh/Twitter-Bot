c10941c94601c3939aa7d3db4ad2d556f8178bc6
renkh <renavoo@gmail.com>
2018-06-11 09:59:47 -0400

    added node_modules and tried to add media check

--- catbot.js
+++ catbot.js
@@ -15,21 +15,30 @@ var params = {
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data) {
+  console.log('Entered gotdata');
   if (!err) {
-  // grab ID of tweet to retweet
-  var retweetId = data.statuses[0].id_str;
-  // Tell TWITTER to retweet
-  T.post('statuses/retweet/:id', {
-      id: retweetId
-  }, function(err, response) {
-      if (response) {
-          console.log('Retweeted!!!');
+    var tweets = data.statuses;
+    console.log('tweets length: ', tweets.length);
+    for (var i = 0; i < tweets.length; i++) {
+      // grab ID of tweet to retweet
+      var retweetId = data.statuses[0].id_str;
+      if (data.entities.media != undefined) {
+        var media = data.entities.media;
+        console.log('Found media');
       }
-      // if there was an error while tweeting
-      if (err) {
-          console.log('Something went wrong while RETWEETING... Duplication maybe...');
-      }
-    });
+      // Tell TWITTER to retweet
+      T.post('statuses/retweet/:id', {
+          id: 0
+      }, function(err, response) {
+          if (response) {
+              console.log('Retweeted!!!');
+          }
+          // if there was an error while tweeting
+          if (err) {
+              console.log('Something went wrong while RETWEETING... Duplication maybe...');
+          }
+        });
+    }
   }
   // if unable to Search a tweet
   else {
