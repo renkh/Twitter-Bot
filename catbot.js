4504a396e00ea3e9b457b51d516660f4633e74c8
renkh <renavoo@gmail.com>
2018-06-14 11:07:08 -0400

    added ability to reply to tweet with media

--- catbot.js
+++ catbot.js
@@ -6,6 +6,9 @@ var config = require('./config.js');
 // Making a Twit object for connection to the API
 var T = new Twit(config);
 
+// For reading image files
+var fs = require('fs');
+
 // Setting up a user stream
 var stream = T.stream('user');
 
@@ -48,15 +51,26 @@ function tweetEvent(tweet) {
     // Get rid of the @ mention
     txt = txt.replace(/@catbot488/g,'');
 
-    // Start a reply back to the sender
-    var replyText = '@'+name + ' ';
-    // Reverse their text
-    for (var i = txt.length-1; i >= 0; i--) {
-      replyText += txt.charAt(i);
-    }
+    // Read the file made by Processing
+    var b64content = fs.readFileSync('cat_pics/' + Math.floor(Math.random()*14) + '.jpg', { encoding: 'base64' })
 
-    // Post that tweet
-    T.post('statuses/update', { status: replyText, in_reply_to_status_id: id}, tweeted);
+    // Upload the media
+    T.post('media/upload', { media_data: b64content }, uploaded);
+
+    function uploaded(err, data, response) {
+
+      // Now we can reference the media and post a tweet
+      // with the media attached
+      var mediaIdStr = data.media_id_string;
+
+      // Start a reply back to the sender
+      var replyText = '@'+name + 'Here is your #kitty pic!';
+
+      console.log(replyText);
+
+      // Post that tweet
+      T.post('statuses/update', { status: replyText, media_ids: [mediaIdStr], in_reply_to_status_id: id}, tweeted);
+    };
 
     // Make sure it worked!
     function tweeted(err, reply) {
